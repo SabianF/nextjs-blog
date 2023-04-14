c1f11f147acd469901d7c1f5146e023783ffbc6d
SabianF <58588133+SabianF@users.noreply.github.com>
2023-04-14 01:57:24 -0700

    feat: adding an image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,17 @@
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function FirstPost() {
   return (
     <main>
       <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpg"
+        width={256}
+        height={256}
+        alt="Sabian Finogwar profile picture"
+      />
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
