d325360a08988d90a334fcefd18306e284a405ac
SabianF <58588133+SabianF@users.noreply.github.com>
2023-04-14 23:50:57 -0700

    feat: add Head to first post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,26 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
-    <main>
-      <h1>First Post</h1>
-      <Image
-        src="/images/profile.jpg"
-        width={256}
-        height={256}
-        alt="Sabian Finogwar profile picture"
-      />
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </main>
+    <div>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <main>
+        <h1>First Post</h1>
+        <Image
+          src="/images/profile.jpg"
+          width={256}
+          height={256}
+          alt="Sabian Finogwar profile picture"
+        />
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </main>
+    </div>
   );
 }
