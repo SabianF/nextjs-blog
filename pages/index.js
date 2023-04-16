38c74b3e8c2491d831e2f4361d10d9fdc2df1153
SabianF <58588133+SabianF@users.noreply.github.com>
2023-04-15 22:12:02 -0700

    feat: loading static local data

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,19 @@ import Link from 'next/link';
 
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -16,8 +27,22 @@ export default function Home() {
         <p>
           This is my sample website.
         </p>
+        <Link href="/posts/first-post">First post</Link>
       </section>
-      <Link href="/posts/first-post">First post</Link>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
