c85f88d560ef01402cb207e3aacc678fe1e2e08e
SabianF <58588133+SabianF@users.noreply.github.com>
2023-04-21 22:49:12 -0700

    refactor: mv code to fns

--- components/layout.js
+++ components/layout.js
@@ -10,62 +10,82 @@ const name = 'Sabian Finogwar';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
-  return (
+  const htmlLayout = (
     <div className={styles.container}>
+      {layoutHead()}
+      {layoutHeader()}
+      {children}
+      {layoutFooter()}
+    </div>
+  );
+
+  return htmlLayout;
+
+  function layoutHead() {
+    return (
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
+          content="Learn how to build a personal website using Next.js" />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
+    );
+  }
+
+  function layoutHeader() {
+    const htmlHeaderChildren = home ? (
+      <>
+        <Image
+          priority
+          src="/images/profile.jpg"
+          className={utilStyles.borderCircle}
+          height={144}
+          width={144}
+          alt="" />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+      </>
+    ) : (
+      <>
+        <Link href="/">
+          <Image
+            priority
+            src="/images/profile.jpg"
+            className={utilStyles.borderCircle}
+            height={108}
+            width={108}
+            alt="" />
+        </Link>
+        <h2 className={utilStyles.headingLg}>
+          <Link href="/" className={utilStyles.colorInherit}>
+            {name}
+          </Link>
+        </h2>
+      </>
+    );
+
+    const htmlHeader = (
       <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
+        {htmlHeaderChildren}
       </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
-        </div>
-      )}
-    </div>
-  );
+    );
+
+    return htmlHeader;
+  }
+
+  function layoutFooter() {
+    const htmlFooter = !home && (
+      <div className={styles.backToHome}>
+        <Link href="/">← Back to home</Link>
+      </div>
+    );
+
+    return htmlFooter;
+  }
 }
