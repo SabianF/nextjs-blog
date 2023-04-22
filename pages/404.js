bf90f22e79038dbc9ce1272de7e1acd44f3d5483
SabianF <58588133+SabianF@users.noreply.github.com>
2023-04-21 22:35:37 -0700

    feat: made custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <h1>Not Found</h1>
+      <p>Whoops! This page doesn't seem to exist</p>
+    </Layout>
+  );
+}
